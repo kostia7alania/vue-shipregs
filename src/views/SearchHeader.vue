b37f352836ef04027e91cb7fd942e73c0ea51c3d
kostia7alania <osetia-alania@mail.ru>
2019-01-31 11:50:22 +0300

    пофиксил траблы с ракктиновтсью

--- src/views/SearchHeader.vue
+++ src/views/SearchHeader.vue
@@ -11,10 +11,14 @@
       <span class="css-typing" v-if="!data.ed_Port">во всех морских портах</span>
     
     </h2>
-    <h6 class="first-h6"> с <animateNumber :value="data.timeFrom.HH" :isTime="true"/>:<animateNumber :value="data.timeFrom.mm" :isTime="true"/> <!--{{data.ed_DateFrom}}--> &nbsp; <animateNumber :value="compDate(0)"  :isTime="true"/>.<animateNumber :value="compDate(1)"  :isTime="true"/>.<animateNumber :value="compDate(2)"/></h6>
-    <h6 class="second-h6" v-if="compDatePlus1()"
-                         >по <animateNumber :value="data.timeTo.HH" :isTime="true"/>:<animateNumber :value="data.timeTo.mm" :isTime="true"/>  <!--{{getTODAY}}-->       &nbsp;    <animateNumber :value="compDatePlus1().dd"  :isTime="true"/>.<animateNumber :value="compDatePlus1().mm"  :isTime="true"/>.<animateNumber :value="compDatePlus1().yy"/></h6>
+
+    <template v-if="data && compDate(2)  && data.ed_DateFrom && compDatePlus1() && data.timeTo.HH"> 
+    <h6 class="first-h6"> с 
+      <animateNumber :value="data.timeFrom.HH" :isTime="true"/>:<animateNumber :value="data.timeFrom.mm" :isTime="true"/> <!--{{data.ed_DateFrom}}--> &nbsp; <animateNumber :value="compDate(0)"  :isTime="true"/>.<animateNumber :value="compDate(1)"  :isTime="true"/>.<animateNumber :value="compDate(2)"/></h6>
+    <h6 class="second-h6"> по
+       <animateNumber :value="data.timeTo.HH" :isTime="true"/>:<animateNumber :value="data.timeTo.mm" :isTime="true"/>  <!--{{getTODAY}}-->       &nbsp;    <animateNumber :value="compDatePlus1().dd"  :isTime="true"/>.<animateNumber :value="compDatePlus1().mm"  :isTime="true"/>.<animateNumber :value="compDatePlus1().yy"/></h6>
     
+    </template>
     
   </div>
 </template>
@@ -41,22 +45,23 @@ export default {
       
       compDatePlus1_comp(){
         let r = this.compDatePlus1();
-        return typeof r === 'object' ?r.dd : '';
+        return typeof r === 'object' ? r.dd : '';
     }
     }, 
     methods: { 
       compDate(num){
         let d = this.data
-        if(typeof d === 'object' && 'ed_DateFrom' in d && typeof d.ed_DateFrom == 'string') return d.ed_DateFrom.split('.')[num];
-        else return '';        
+        if(typeof d === 'object' && 'ed_DateFrom' in d && typeof d.ed_DateFrom == 'string') 
+             return d.ed_DateFrom.split('.')[num];
+        //else return ''; 
       },
       compDatePlus1(){
         let d = this.data
         if(typeof d === 'object' && 'ed_DateFrom' in d && typeof d.ed_DateFrom == 'string') {
           let dd = d.ed_DateFrom.split('.')[0];
           let mm = d.ed_DateFrom.split('.')[1];
-          let yy = d.ed_DateFrom.split('.')[2];
-          let date = new Date(`${mm}.${dd}.${yy}`);
+          let yy = d.ed_DateFrom.split('.')[2]; 
+          let date = new Date(`${yy}-${mm}-${dd}`); 
           date.setDate(date.getDate() + 1); 
           return { dd:date.getDate(),mm:date.getMonth()+1,yy:date.getFullYear() };
         }
